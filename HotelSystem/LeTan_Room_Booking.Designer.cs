4c9ebbc0d78f2b136e10ebe01eaa42fea872aa85
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2023-06-19 00:09:23 +0700

    Fix font

--- HotelSystem/LeTan_Room_Booking.Designer.cs
+++ HotelSystem/LeTan_Room_Booking.Designer.cs
@@ -54,7 +54,7 @@ namespace HotelSystem
             // 
             this.HomeLabel.AutoSize = false;
             this.HomeLabel.BackColor = System.Drawing.Color.Transparent;
-            this.HomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeLabel.Font = new System.Drawing.Font("Google Sans", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HomeLabel.ForeColor = System.Drawing.Color.DeepPink;
             this.HomeLabel.Location = new System.Drawing.Point(0, 34);
             this.HomeLabel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -72,7 +72,7 @@ namespace HotelSystem
             this.quitBookingBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.quitBookingBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.quitBookingBtn.FillColor = System.Drawing.Color.DeepPink;
-            this.quitBookingBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.quitBookingBtn.Font = new System.Drawing.Font("Google Sans", 9F);
             this.quitBookingBtn.ForeColor = System.Drawing.Color.White;
             this.quitBookingBtn.Location = new System.Drawing.Point(991, 111);
             this.quitBookingBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -85,7 +85,7 @@ namespace HotelSystem
             // roomBookingLabel
             // 
             this.roomBookingLabel.BackColor = System.Drawing.Color.Transparent;
-            this.roomBookingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomBookingLabel.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.roomBookingLabel.Location = new System.Drawing.Point(55, 212);
             this.roomBookingLabel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.roomBookingLabel.Name = "roomBookingLabel";
@@ -103,7 +103,7 @@ namespace HotelSystem
             this.customerId.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
             this.customerId.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
             this.customerId.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.customerId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerId.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.customerId.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.customerId.Location = new System.Drawing.Point(55, 247);
             this.customerId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -117,7 +117,7 @@ namespace HotelSystem
             // guna2HtmlLabel1
             // 
             this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel1.Location = new System.Drawing.Point(879, 370);
             this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
@@ -128,7 +128,7 @@ namespace HotelSystem
             // guna2HtmlLabel2
             // 
             this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel2.Location = new System.Drawing.Point(480, 212);
             this.guna2HtmlLabel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
@@ -139,7 +139,7 @@ namespace HotelSystem
             // guna2HtmlLabel3
             // 
             this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel3.Location = new System.Drawing.Point(55, 370);
             this.guna2HtmlLabel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
@@ -151,7 +151,7 @@ namespace HotelSystem
             // guna2HtmlLabel4
             // 
             this.guna2HtmlLabel4.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel4.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel4.Location = new System.Drawing.Point(480, 370);
             this.guna2HtmlLabel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel4.Name = "guna2HtmlLabel4";
@@ -162,7 +162,7 @@ namespace HotelSystem
             // guna2HtmlLabel5
             // 
             this.guna2HtmlLabel5.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel5.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel5.Location = new System.Drawing.Point(55, 511);
             this.guna2HtmlLabel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel5.Name = "guna2HtmlLabel5";
@@ -180,7 +180,7 @@ namespace HotelSystem
             this.specialRequest.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
             this.specialRequest.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
             this.specialRequest.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.specialRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.specialRequest.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.specialRequest.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.specialRequest.Location = new System.Drawing.Point(55, 546);
             this.specialRequest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -198,7 +198,7 @@ namespace HotelSystem
             this.addRoomBookingBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.addRoomBookingBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.addRoomBookingBtn.FillColor = System.Drawing.Color.DeepPink;
-            this.addRoomBookingBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.addRoomBookingBtn.Font = new System.Drawing.Font("Google Sans", 9F);
             this.addRoomBookingBtn.ForeColor = System.Drawing.Color.White;
             this.addRoomBookingBtn.Location = new System.Drawing.Point(991, 783);
             this.addRoomBookingBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -211,7 +211,7 @@ namespace HotelSystem
             // guna2HtmlLabel6
             // 
             this.guna2HtmlLabel6.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel6.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel6.Location = new System.Drawing.Point(882, 212);
             this.guna2HtmlLabel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel6.Name = "guna2HtmlLabel6";
@@ -226,7 +226,7 @@ namespace HotelSystem
             this.checkoutDate.Checked = true;
             this.checkoutDate.CustomFormat = "yyyy-MM-dd";
             this.checkoutDate.FillColor = System.Drawing.Color.White;
-            this.checkoutDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkoutDate.Font = new System.Drawing.Font("Google Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkoutDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.checkoutDate.Location = new System.Drawing.Point(480, 405);
             this.checkoutDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
@@ -242,7 +242,7 @@ namespace HotelSystem
             this.checkinDate.Checked = true;
             this.checkinDate.CustomFormat = "yyyy-MM-dd";
             this.checkinDate.FillColor = System.Drawing.Color.White;
-            this.checkinDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkinDate.Font = new System.Drawing.Font("Google Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkinDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.checkinDate.Location = new System.Drawing.Point(55, 405);
             this.checkinDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
@@ -259,7 +259,7 @@ namespace HotelSystem
             this.roomId.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.roomId.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.roomId.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.roomId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomId.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.roomId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
             this.roomId.ItemHeight = 30;
             this.roomId.Location = new System.Drawing.Point(882, 247);
@@ -276,7 +276,7 @@ namespace HotelSystem
             this.roomType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.roomType.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.roomType.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.roomType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.roomType.Font = new System.Drawing.Font("Google Sans", 10.8F);
             this.roomType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
             this.roomType.ItemHeight = 30;
             this.roomType.Items.AddRange(new object[] {
@@ -296,7 +296,7 @@ namespace HotelSystem
             this.bookingDate.CustomFormat = "yyyy-MM-dd";
             this.bookingDate.Enabled = false;
             this.bookingDate.FillColor = System.Drawing.Color.White;
-            this.bookingDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bookingDate.Font = new System.Drawing.Font("Google Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bookingDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.bookingDate.Location = new System.Drawing.Point(882, 405);
             this.bookingDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
@@ -314,7 +314,7 @@ namespace HotelSystem
             this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2ComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Google Sans", 10.8F);
             this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
             this.guna2ComboBox1.ItemHeight = 30;
             this.guna2ComboBox1.Location = new System.Drawing.Point(882, 546);
@@ -326,7 +326,7 @@ namespace HotelSystem
             // guna2HtmlLabel7
             // 
             this.guna2HtmlLabel7.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel7.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel7.Location = new System.Drawing.Point(882, 511);
             this.guna2HtmlLabel7.Margin = new System.Windows.Forms.Padding(4);
             this.guna2HtmlLabel7.Name = "guna2HtmlLabel7";
